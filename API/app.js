2aa46f6d68ec9befcd3948995981d16766eaeb69
unknown <danielamonica587@gmail.com>
2023-10-24 20:39:37 +0300

    mda

--- API/app.js
+++ API/app.js
@@ -19,6 +19,9 @@ const app = express();
 const logger = require('morgan');
 const cors = require('cors');
 const port = 3000;
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const wellKeptSecret = "Mos Craciun NU exista!"
 
 app.use(logger('dev'));
 app.use(cors()) //see more at https://www.npmjs.com/package/cors
@@ -36,11 +39,34 @@ app.post('/register', (req, res) => {
   if(filterdUsers.length!==0){
     res.status(400).json({message: "user already exists!"});
   }else{
-    let nextId = users[users.length-1].id + 1
-    user.id = nextId
-    users.push(user)
-    console.log("DB has been updated!", users)
-    res.send('Successfully added!')
+    bcrypt.hash(user.password, 10, (err, hash)=>{
+        let nextId = users[users.length-1].id + 1
+        user.id = nextId
+        user.password = hash
+        users.push(user)
+        console.log("DB has been updated!", users)
+        res.send('Successfully added!')
+    })
+  }
+});
+
+app.post('/login', (req, res) => {
+  let user = req.body
+  console.log('trying to login with: ', user)
+  const filterdUsers = users.filter(u=>u.email===user.email)
+  if(!filterdUsers.length){
+    res.status(400).json({message: "user doesn't exist!"});
+  }else{
+    let dbHash = filterdUsers[0].password
+    bcrypt.compare(user.password, dbHash, (err, result)=>{
+        if(result) {
+            const token = jwt.sign({email: user.email},wellKeptSecret)
+            let response = {token, success : true}
+            res.status(200).json(response)
+        }else{
+            res.status(400).json({message: "password mismatch!"});
+        }
+    })
   }
 });
 
